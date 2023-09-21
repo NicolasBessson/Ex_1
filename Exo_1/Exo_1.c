df7869c56009ca93eca48bf5fc6e6b66924b8db8
ETMLNET\nicbesson <pd37qyn@eduvaud.ch>
2023-09-21 09:31:55 +0200

    Version_1_21.09.2023

---
+++ Exo_1/Exo_1.c
@@ -0,0 +1,27 @@
+//////////////////////////////////
+// Exo_1
+// Developed: ABR,KGR,NBN
+// Date:14.09.2023
+//////////////////////////////////
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+	int ValDec = 0;
+
+	while (1)
+	{
+		printf("Entrez une valeur entre 0 et 255\n");
+		scanf_s("%d", &ValDec);
+		printf("Valeur Entree : %d\n", ValDec);
+		if (ValDec > 0 || ValDec < 255)
+		{
+
+		}
+
+	}
+
+}
